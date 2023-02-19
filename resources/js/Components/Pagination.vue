89fae4fbd076aec581f39ea96c3a9941ee9853ae
Virgilio Tapia Jr <vtapia.jr@gmail.com>
2023-02-19 22:16:52 +0800

    change pagination page numbers active, hover color and background

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -10,13 +10,12 @@
 </script>  
   
 <template>
-    <div v-if="data.length > 3" class="flex justify-center mt-4 space-x-4">      
-      
+    <div v-if="data.length > 3" class="flex justify-center mt-4 space-x-4">            
       <Link
           v-for="(link, k) in data"
           :key="k"
-          class="px-4 py-3 text-sm leading-4 bg-white rounded hover:bg-white focus:text-indigo-500 hover:shadow"
-          :class="{'bg-blue-400 text-white font-bold': link.active}"
+          class="px-4 py-3 text-sm leading-4 bg-white rounded hover:bg-black hover:text-white focus:text-white hover:shadow"
+          :class="{'bg-lightseagreen-400 text-white font-bold': link.active}"
           :href="link.url"
           v-html="link.label"
       />
